c2a80b67036114165789872f57b84af9e1e8a7aa
Manikant-git4151 <2025415141.manikant@ug.sharda.ac.in>
2026-03-24 16:10:44 +0530

    24-03-2026

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,31 @@
-var a = prompt("whats your age ?")
-if (a > 18) {
+// var a = prompt("whats your age ?")
+// if (a > 18) 
+//     {
+//      var b = prompt("whats your name : ")   
+//     alert("hello " + b + "  you are an adult ")
+// }
+// else {
+//     alert("nabaalik!!!!!")
 
-    alert("user you are an adult ")
-}
-else {
-    alert("nabaalik!!!!!")
-}
+// }
+// console.log("your age is " +a+ " this is a normal console output ")
+
+var a =10;
+var qa =true;
+console.log(a);
+
+// let b =20
+// let b;   
+// shows error
+
+let f=true;
+f=true;
+console.log(f);
+
+var d=7;
+d=10;
+console.log(d); 
+
+const pi=3.14159;
+//pi=3;    // will give error in consoel;
+console.log(pi);
